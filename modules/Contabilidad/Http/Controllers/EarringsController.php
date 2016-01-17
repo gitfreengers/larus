978848c2dc71126ed8e1626382d8df5729fc6bf6
gitfreengers <gitfreengers>
2016-01-16 20:18:29 -0600

    agregacion de pendientes

--- modules/Contabilidad/Http/Controllers/EarringsController.php
+++ modules/Contabilidad/Http/Controllers/EarringsController.php
@@ -2,13 +2,32 @@
 
 use Pingpong\Modules\Routing\Controller;
 use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Modules\User\Entities\User;
+use Illuminate\Support\Facades\DB;
+use Modules\Contabilidad\Entities\Sales;
 
 class EarringsController extends Controller {
+	
+	protected $user_auth;
+	
+	public function __construct(){
+		$this->user_auth = Sentinel::getUser();
+	}
 	
 	public function index()
 	{
 		if(Sentinel::hasAccess('pendientes.view')){
-			return view('contabilidad::Earrings.index');
+			$usuario = User::find($this->user_auth->id);
+			$oficinas = array();
+			foreach ($usuario->plazas as $plazas){
+				array_push($oficinas, $plazas->Oficina);
+			}
+			DB::enableQueryLog();
+			$ventasPendientes = Sales::select('*',  DB::raw('SUM(ammount) as ammount, SUM(ammount_applied) as ammount_applied '))
+								->whereRaw('credit_debit = ?  ', ['credit'])
+								->groupBy('reference')
+								->get();
+			return view('contabilidad::Earrings.index', compact("ventasPendientes"));
 		}else{
 			alert()->error('No tiene permisos para acceder a esta area.', 'Oops!')->persistent('Cerrar');
 			return back();
--- modules/Contabilidad/Resources/views/Earrings/index.blade.php
+++ modules/Contabilidad/Resources/views/Earrings/index.blade.php
@@ -12,7 +12,7 @@
         
         	<div class="box-header">
             	<div class="btn-group pull-right">
-                	<a href="{{route('contabilidad.pendientes.create')}}" class="btn btn-success btn-flat"><i class="fa fa-money"></i> Depositos</a>
+                	<a href="{{route('contabilidad.depositos.index')}}" class="btn btn-success btn-flat"><i class="fa fa-money"></i> Depositos</a>
 				</div>
                 <div class="col-lg-10">
                 	@include('flash::message')
@@ -23,46 +23,29 @@
                     <thead>
                     <tr>
                         <th>Fecha</th>
-                        <th>Tipo de movimiento</th>
-                        <th>Persona</th>
                         <th>Referencia</th>
-                        <th>Conciliar</th>
+                        <th>Factura UUID</th>
+                        <th>Monto</th>
+                        <th>Monto aplicado</th>
                     </tr>
                     </thead>
                     <tbody>
-                        <tr>
-	                        <td>15/12/2015</td>
-	                        <td>Factura: xxxx</td>
-	                        <td>Cliente 1</td>
-	                        <td>TXC0900990</td>
-	                        <td align="center"> 
-	                        	<div class="btn-group">
-									<a  href="#" class="btn btn-info btn-flat" title="Conciliar"><i class="fa fa-check-square-o "></i> Conciliar</a>
-                                </div>
-							</td>   
-                        </tr>
-                        <tr>
-	                        <td>15/12/2015</td>
-	                        <td>Deposito </td>
-	                        <td>Bancomer</td>
-	                        <td>Aut. 445</td>
-	                        <td align="center"> 
-	                        	<div class="btn-group">
-									<a  href="#" class="btn btn-info btn-flat" title="Conciliar"><i class="fa fa-check-square-o "></i> Conciliar</a>
-                                </div>
-							</td>   
-                        </tr>
-                        <tr>
-	                        <td>15/12/2015</td>
-	                        <td>Factura: xxxx</td>
-	                        <td>Cliente B</td>
-	                        <td>TAC90843</td>
-	                        <td align="center"> 
-	                        	<div class="btn-group">
-									<a  href="#" class="btn btn-info btn-flat" title="Conciliar"><i class="fa fa-check-square-o "></i> Conciliar</a>
-                                </div>
-							</td>     
-                        </tr>
+                    	@foreach($ventasPendientes as $ventas)
+                    		<tr>
+                    			<td> {{$ventas->date}} </td>
+                    			<td> {{$ventas->reference}} </td>
+                    			<td> {{$ventas->factura_uuid}} </td>
+                    			<td align="right"> $ {{number_format($ventas->ammount, 2)}} </td>
+                    			<td align="right"> $ {{number_format($ventas->ammount_applied, 2)}} </td>
+								<!--	
+                    			<td align="center"> 
+		                        	<div class="btn-group">
+										<a  href="#" class="btn btn-info btn-flat" title="Conciliar"><i class="fa fa-check-square-o "></i> Conciliar</a>
+	                                </div>
+								</td>
+								-->
+                    		</tr>
+                    	@endforeach                        
                     </tbody>
                 </table>
             </div><!-- /.box-body -->
@@ -75,7 +58,13 @@
     <script src="{{asset('bower_components/admin-lte/plugins/datatables/jquery.dataTables.min.js') }}" type="text/javascript"></script>
     <script src="{{asset('bower_components/admin-lte/plugins/datatables/dataTables.bootstrap.min.js') }}" type="text/javascript"></script>
     <!-- confirmation -->
-    <script src="{{asset('js/ajax.js')}}"></script>
+    
     <script src="{{asset('bower_components/admin-lte/plugins/bootstrap-confirmation/bootstrap-confirmation.min.js') }}" type="text/javascript"></script>
-
+	<script>
+	tabla = $("#datatable").DataTable({
+        "language": {
+            "url": '{!! asset("bower_components/admin-lte/plugins/datatables/lang/spanish.json") !!}'
+        },
+	});
+	</script>
 @endsection
